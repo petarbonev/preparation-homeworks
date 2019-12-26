e361cf355425a01076d1b7f1f0c666993f1c9b42
Petar Bonev <bonev09@abv.bg>
2019-12-26 17:16:57 +0200

    remove empty spaces

--- Homework Types and Variables/CirclePerimeterAndArea.js
+++ Homework Types and Variables/CirclePerimeterAndArea.js
@@ -1,7 +1,7 @@
 var input = [
-    '3.5', 
+    '3.5',
 ];
- 
+
 var print = this.print || console.log;
 var gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
@@ -11,5 +11,5 @@ let radius = gets();
 let perimeter = 2 * Math.PI * radius;
 let area = Math.PI * Math.pow(radius, 2);
 
-print('The Perimeter is: ' + perimeter.toFixed(2));
+print('The Perimeter is : ' + perimeter.toFixed(2));
 print('The Area is: ' + area.toFixed(2));
